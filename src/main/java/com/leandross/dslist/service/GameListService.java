39f14534740192eff97f831b77dde162a4ce76da
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2025-01-25 18:22:17 -0300

    atualização

--- src/main/java/com/leandross/dslist/service/GameListService.java
+++ src/main/java/com/leandross/dslist/service/GameListService.java
@@ -7,7 +7,6 @@ import com.leandross.dslist.repository.GameListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 
 @Service
